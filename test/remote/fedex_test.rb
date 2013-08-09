eed86b18212fa07eb7f6dabeabbc7a2ee2a17dfc
Timur Yalcin <tiyalcin@gmail.com>
2013-08-09 18:21:24 +0200

    envelope parameter

--- test/remote/fedex_test.rb
+++ test/remote/fedex_test.rb
@@ -26,7 +26,7 @@ class FedExTest < Test::Unit::TestCase
     recipient = ActiveMerchant::Shipping::ShippingRecipient.new(address: address_rec, contact: contact_recipient)       
     
     fedex = FedEx.new(key: 'rscqm75MLampLUuV', password: '8rTZHQ6vbyOsGOgtwMXrZ1kIU', account: '510087267', login: '118511895', test: true)
-    response = fedex.find_rates(shipper, recipient, packages)
+    response = fedex.find_rates(shipper, recipient, packages, false)
     
     fedex_rates = response.rates.sort_by(&:price).collect {|rate| 
       [rate.service_name, rate.price]
@@ -50,7 +50,7 @@ class FedExTest < Test::Unit::TestCase
     recipient = ActiveMerchant::Shipping::ShippingRecipient.new(address: address_rec, contact: contact_recipient)       
     
     fedex = FedEx.new(key: 'rscqm75MLampLUuV', password: '8rTZHQ6vbyOsGOgtwMXrZ1kIU', account: '510087267', login: '118511895', test: true)
-    response = fedex.find_rates(shipper, recipient, packages)
+    response = fedex.find_rates(shipper, recipient, packages, false)
     
     fedex_rates = response.rates.sort_by(&:price).collect {|rate| 
       [rate.service_name, rate.price]
@@ -76,7 +76,7 @@ class FedExTest < Test::Unit::TestCase
     recipient = ActiveMerchant::Shipping::ShippingRecipient.new(address: address_rec, contact: contact_recipient)       
     
     fedex = FedEx.new(key: 'rscqm75MLampLUuV', password: '8rTZHQ6vbyOsGOgtwMXrZ1kIU', account: '510087267', login: '118511895', test: true)
-    response = fedex.find_rates(shipper, recipient, packages)
+    response = fedex.find_rates(shipper, recipient, packages, false)
     
     fedex_rates = response.rates.sort_by(&:price).collect {|rate| 
       puts rate.currency
